c48525083fc10c8047ca32216bfdbb38d52aa972
TheBoJeu <louis.leenar@gmail.com>
2021-12-08 12:21:03 +0100

    added figure.hpp

--- include/figure.hpp
+++ include/figure.hpp
@@ -5,4 +5,16 @@
 #ifndef PROJET_COO_FIGURE_HPP
 #define PROJET_COO_FIGURE_HPP
 
+#include "dice.hpp"
+
+namespace yahtzee::figures {
+    class figure {
+    protected:
+        int _points = 0;
+    public:
+        virtual void parse(yahtzee::dice dices[]) = 0;
+        int get_points();
+    };
+}
+
 #endif //PROJET_COO_FIGURE_HPP
--- include/sup_part.hpp
+++ include/sup_part.hpp
@@ -5,4 +5,8 @@
 #ifndef PROJET_COO_SUP_PART_HPP
 #define PROJET_COO_SUP_PART_HPP
 
+namespace yahtzee::figures {
+
+}
+
 #endif //PROJET_COO_SUP_PART_HPP
