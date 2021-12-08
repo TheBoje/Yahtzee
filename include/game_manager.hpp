3f819dcc6680124e8336e8e3a0ba83e7cbaf5e4e
TheBoJeu <louis.leenar@gmail.com>
2021-12-08 11:58:01 +0100

    added hpp for gm & level

--- include/game_manager.hpp
+++ include/game_manager.hpp
@@ -5,4 +5,19 @@
 #ifndef PROJET_COO_GAME_MANAGER_HPP
 #define PROJET_COO_GAME_MANAGER_HPP
 
+#include <vector>
+
+namespace yahtzee {
+    class game_manager {
+    private:
+        std::vector<yahtzee::entity::players> _players;
+        yahtzee::difficulty::level _level;
+        yahtzee::roll _roll;
+
+    public:
+        game_manager(yahtzee::difficulty::level level);
+        bool turn();
+    };
+}
+
 #endif //PROJET_COO_GAME_MANAGER_HPP
--- include/level.hpp
+++ include/level.hpp
@@ -5,4 +5,16 @@
 #ifndef PROJET_COO_LEVEL_HPP
 #define PROJET_COO_LEVEL_HPP
 
+#include <vector>
+
+namespace yahtzee::difficulty {
+    class level {
+    protected:
+        std::vector<yahtzee::entity::players> _players;
+    public:
+        level(std::vector<yahtzee::entity::players> players); // TODO(Louis): Ca fait une copie du vecteur ou un passage par référence?
+        virtual void turn() = 0;
+    };
+}
+
 #endif //PROJET_COO_LEVEL_HPP
