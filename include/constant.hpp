d07a7c4d6f788744aa42181f61c35918a9bcd689
Altimors <vincent.commin2000@orange.fr>
2021-12-08 13:14:25 +0100

    Added :  - constant file  - dice.hpp  - roll.hpp
    Updated :
     - CMakeLists.txt : added constant.hpp

---
+++ include/constant.hpp
@@ -0,0 +1,12 @@
+//
+// Created by altimor on 08/12/2021.
+//
+
+#ifndef PROJET_COO_CONSTANT_HPP
+#define PROJET_COO_CONSTANT_HPP
+
+namespace yahtzee {
+    constexpr int NB_DICE = 5;
+}
+
+#endif //PROJET_COO_CONSTANT_HPP
--- include/roll.hpp
+++ include/roll.hpp
@@ -5,4 +5,29 @@
 #ifndef PROJET_COO_ROLL_HPP
 #define PROJET_COO_ROLL_HPP
 
+#include <iostream>
+
+#include "dice.hpp"
+#include "constant.hpp"
+
+namespace yahtzee {
+    class roll {
+        dice _dices[NB_DICE];
+        bool _kept[NB_DICE];
+
+    public:
+        roll();
+
+        /**
+         * Roll all the non-kept dices
+         */
+        void update();
+
+        /**
+         * Reset the kept array by setting all the values to false
+         */
+        void reset();
+    };
+}
+
 #endif //PROJET_COO_ROLL_HPP
