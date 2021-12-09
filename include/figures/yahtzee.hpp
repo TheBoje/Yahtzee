20542238e37e545034cb0a638fe3219875aa9e72
TheBoJeu <louis.leenar@gmail.com>
2021-12-09 01:06:43 +0100

    fix cmakelist file gathering and yahtzee type

--- include/figures/yahtzee.hpp
+++ include/figures/yahtzee.hpp
@@ -2,7 +2,7 @@
 // Created by vince on 08/12/2021.
 //
 
-#ifndef PROJET_COO_YAHTSEE_HPP
-#define PROJET_COO_YAHTSEE_HPP
+#ifndef PROJET_COO_YAHTZEE_HPP
+#define PROJET_COO_YAHTZEE_HPP
 
-#endif //PROJET_COO_YAHTSEE_HPP
+#endif //PROJET_COO_YAHTZEE_HPP
